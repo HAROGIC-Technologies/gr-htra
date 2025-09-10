6a78996baa718119167a377c9d05999c8c672ad4
HAROGIC Technologies <support@harogic.com>
2025-09-10 17:54:05 +0800

    Update htra_source.h

--- include/htra_device/htra_source.h
+++ include/htra_device/htra_source.h
@@ -6,13 +6,29 @@
 namespace gr {
 namespace htra_device {
 
+enum class DecimationFactor {
+    DECIM_1 = 1,
+    DECIM_2 = 2,
+    DECIM_4 = 4,
+    DECIM_8 = 8,
+    DECIM_16 = 16,
+    DECIM_32 = 32,
+    DECIM_64 = 64,
+    DECIM_128 = 128,
+    DECIM_256 = 256,
+    DECIM_512 = 512,
+    DECIM_1024 = 1024,
+    DECIM_2048 = 2048,
+    DECIM_4096 = 4096,
+};
+
 class htra_source : virtual public gr::sync_block
 {
 public:
     typedef std::shared_ptr<htra_source> sptr;
 
 
-    static sptr make(float center_freq,float sample_rate, float decim_factor, float ref_level);  
+    static sptr make(float center_freq,float sample_rate,  DecimationFactor decim_factor , float ref_level);  
 
   
     virtual gr::basic_block_sptr to_basic_block() = 0;
@@ -20,7 +36,8 @@ public:
     virtual void set_sample_rate(float rate) = 0;
     virtual void set_center_freq(float freq) = 0;
     virtual void set_ref_level(float level) = 0;
-    virtual void set_decim_factor(float decim) = 0;
+
+    virtual void set_decim_factor(DecimationFactor decim) = 0;
 
 
     virtual int activateStream() = 0;
